c760f459fdfc0b754e6271fb03bbc6b83e441d89
Edd Wilder-James <edd@me.com>
2018-12-02 10:12:44 -0800

    Now I remember zipmap!

--- solutions/src/day2/core.clj
+++ solutions/src/day2/core.clj
@@ -13,20 +13,19 @@
 ;;
 ;; make tuple pairs of positions, locate only those pairs with one
 ;; difference.
-(defn positions [s] (set (partition 2 (interleave (range) s))))
 
-(def perms [l]
+(defn pairwise-perms [l]
   (loop [l l a []]
     (let [m (first l) n (rest l)]
       (if (empty? n) a
           (recur n (concat a (partition 2 (interleave (repeat m) n))))))))
 
-(defn diffs [[x y]] (count (set/difference (positions x) (positions y))))
+(defn positions [s] (set (partition 2 (interleave (range) s))))
+(defn diff-count [[x y]] (count (set/difference (positions x) (positions y))))
 
 (defn find-answer [l]
-  (let [p (perms l)
-        [c1 c2] (second (first (filter (fn [[x y]] (= x 1))
-                                       (partition 2 (interleave (map diffs p) p)))))
+  (let [p (pairwise-perms l)
+        [c1 c2] ((zipmap (map diff-count p) p) 1)
         c (first (set/difference (set (vec c1)) (set (vec c2))))]
     (str/replace c1 (str c) "")))
 
