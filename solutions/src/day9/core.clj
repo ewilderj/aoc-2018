7849ac18d5b6e4c7cddd0801a783efe5c7883be6
Edd Wilder-James <edd@me.com>
2018-12-17 08:42:38 -0800

    Finally a performant version for part 2 day 9

--- solutions/src/day9/core.clj
+++ solutions/src/day9/core.clj
@@ -31,7 +31,7 @@
     )
 
 ;; 410 players, marble 72059 -> 429287
-;; 410 players, marble 7205900 -> ?????
+;; 410 players, marble 7205900 -> 3624387659
 
 ;; let's make a circular buffer where it's relatively efficient
 ;; to rotate
@@ -54,7 +54,7 @@
 
 (defn cplay [players max-marble]
   (loop [c {:l [0] :r [1]} m 2 scores {}]
-    (if (= 0 (mod m 1000)) (println "marble" m))
+    (if (= 0 (mod m 10000)) (println "marble" m))
     (if (> m max-marble) (apply max (vals scores)) ; terminate game at max-marble
         (if (= 0 (mod m 23))
           (let [pn (mod m players)]
@@ -68,6 +68,68 @@
             (recur
              (craddl c m)
              (inc m) scores)
+            ))
+        ))
+  )
+
+;; version which uses something like a linked list
+
+(def lring {0 {:n 0 :p 0}})
+
+(defn add-after [ring where item]
+  (let [n ((ring where) :n)
+        p ((ring where) :p)]
+    (-> ring
+        (assoc-in [where :n] item)
+        (assoc-in [n :p] item)
+        (assoc-in [item :n] n)
+        (assoc-in [item :p] where))
+    ))
+
+(defn ring-remove [ring item]
+  (let [p ((ring item) :p)
+        n ((ring item) :n)]
+    (-> (dissoc ring item)
+        (assoc-in [p :n] n)
+        (assoc-in [n :p] p)
+        )
+    )
+  )
+
+(defn back-n [ring item n]
+  (if (<= n 0) item
+      (recur ring (get-in ring [item :p]) (dec n))))
+
+(defn trav [ring item]
+  (loop [r ring i item l []]
+    (if (empty? r) l
+        (recur (dissoc r i)
+               (get-in r [i :n])
+               (conj l i))
+        )
+    ))
+
+(defn rplay [players max-marble]
+  (loop [c lring m 1 z 0 scores {}]
+    ;; (if (= 0 (mod m 1000)) (println "marble" m))
+    (if (> m max-marble) (apply max (vals scores)) ; terminate game at max-marble
+        (if (= 0 (mod m 23))
+          (let [pn (mod m players)
+                winning (back-n c z 8)
+                nc (ring-remove c winning)]
+;;            (println "player wins" m "and" winning "nc" (trav nc 0))
+            (recur nc
+                   (inc m)
+                   (back-n c z 6)
+                   (assoc scores pn (+ (scores pn 0) m winning))))
+          ;; normal move
+          (do
+;;            (println "c" (trav c 0) "m" m)
+            (recur
+             (add-after c z m)
+             (inc m)
+             (get-in c [z :n])
+             scores)
             ))
         ))
   )
