0cbeab234a9c7fbabff8ed380eb14f3eb0444fc1
Edd Wilder-James <edd@me.com>
2018-12-16 18:32:09 -0800

    Added a faster version that uses two vectors

--- solutions/src/day9/core.clj
+++ solutions/src/day9/core.clj
@@ -15,12 +15,13 @@
                 winning (first nl)
                 pn (mod m players)
                 nsc (assoc scores pn (+ (scores pn 0) m winning))]
-          ;; (println "player" pn "wins" m "and" winning "nl" nl)
+ ;;          (println "player" pn "wins" m "and" winning "nl" nl)
             (recur (rotate (subvec nl 1))
                    (inc m) nsc)
             )
           ;; normal move
-            (do ; (println "p#" (inc pn) "l" l "m" m(defn rotate [v]
+          (do
+ ;;            (println "l" l "m" m)
               (recur
                ;;    (rotate (into [m] l))
                (conj (subvec l 1) m (first l))
@@ -31,3 +32,42 @@
 
 ;; 410 players, marble 72059 -> 429287
 ;; 410 players, marble 7205900 -> ?????
+
+;; let's make a circular buffer where it's relatively efficient
+;; to rotate
+
+;; add a marble and rotate onward
+(defn craddl [c i]
+  {:l (conj (subvec (c :l) 1) i (first (c :r)))
+   :r (conj (subvec (c :r) 1) (first (c :l)))})
+
+;; drop the marble from 9 back, and rotate on 2
+(defn cdandr [c]
+  (let [l (c :l)
+        n (- (count l) 9)]
+    {:l (into (subvec l 1 n) (subvec l (inc n) (+ 3 n)))
+     :r (conj (into (subvec l (+ n 3)) (c :r)) (first l))}))
+
+;; the winning marble is 9 back from the end
+(defn win9 [c]
+  (nth (c :l) (- (count (c :l)) 9)))
+
+(defn cplay [players max-marble]
+  (loop [c {:l [0] :r [1]} m 2 scores {}]
+    (if (= 0 (mod m 1000)) (println "marble" m))
+    (if (> m max-marble) (apply max (vals scores)) ; terminate game at max-marble
+        (if (= 0 (mod m 23))
+          (let [pn (mod m players)]
+            ;;(println "player wins" m "and" (win9 c) "nc" (cdandr c))
+            (recur (cdandr c)
+                   (inc m)
+                   (assoc scores pn (+ (scores pn 0) m (win9 c)))))
+          ;; normal move
+          (do
+            ; (println "c" c "m" m)
+            (recur
+             (craddl c m)
+             (inc m) scores)
+            ))
+        ))
+  )
