39b1730c2977fc7e068409d7fc66baccce1c5071
Edd Wilder-James <edd@me.com>
2018-12-23 09:01:27 -0800

    Solved part 2

--- solutions/src/day14/core.clj
+++ solutions/src/day14/core.clj
@@ -2,6 +2,7 @@
   (:require [aoc.core :as aoc] [clojure.string :as str]
             [clojure.set :as set]))
 
+;; split a number or string into arithmetic digits
 (defn new-recipes [score]
   (->> score
        (str)
@@ -10,39 +11,28 @@
 
 (defn play [max-r]
   (loop [recipes [3 7] elf1 0 elf2 1]
-    ;; (println recipes elf1 elf2)
     (if (>= (count recipes) (+ 10 max-r))
       (apply str (take 10 (drop max-r recipes)))
       (let [s1 (nth recipes elf1) s2 (nth recipes elf2)
             sc (+ s1 s2)
             nr (into recipes (new-recipes sc))
-            nn (count nr)
-            ]
-        (recur nr
-               (mod (+ 1 s1 elf1) nn)
-               (mod (+ 1 s2 elf2) nn))
-      )
-    )
-  ))
+            nn (count nr)]
+        (recur nr (mod (+ 1 s1 elf1) nn) (mod (+ 1 s2 elf2) nn))))))
 
 ;; part1: (play 556061) => "2107929416"
 
 (defn play2 [target]
-  (let [tseq (new-recipes target)]
+  (let [tseq (new-recipes target)    ;; sequence to scan for
+        tn (inc (count tseq))]       ;; check the last N+1 as we can add up to 2 digits
     (loop [recipes [3 7] elf1 0 elf2 1]
-      (if (>= (java.util.Collections/indexOfSubList recipes tseq) 0)
-        (java.util.Collections/indexOfSubList recipes tseq)
-        (let [s1 (nth recipes elf1) s2 (nth recipes elf2)
-              sc (+ s1 s2)
-              nr (into recipes (new-recipes sc))
-              nn (count nr)
-              ]
-          (recur nr
-                 (mod (+ 1 s1 elf1) nn)
-                 (mod (+ 1 s2 elf2) nn))
-      ))
-    )
-  ))
+      (let [checkfor (subvec recipes (max 0 (- (count recipes) tn)))]
+        (if (<= 0 (java.util.Collections/indexOfSubList checkfor tseq))
+          (- (count recipes) tn)
+          (let [s1 (nth recipes elf1) s2 (nth recipes elf2)
+                sc (+ s1 s2)
+                nr (into recipes (new-recipes sc))
+                nn (count nr)]
+            (recur nr (mod (+ 1 s1 elf1) nn) (mod (+ 1 s2 elf2) nn))))))))
 
-;; part2: (play2 556061) =>
+;; part2: (play2 556061) => 20307394
 
