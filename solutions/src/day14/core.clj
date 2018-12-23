42ffb84f14b945d734e801a7c3bfb1fdf9fe7c3a
Edd Wilder-James <edd@me.com>
2018-12-22 17:24:55 -0800

    Naive solution to day 14

--- solutions/src/day14/core.clj
+++ solutions/src/day14/core.clj
@@ -2,3 +2,47 @@
   (:require [aoc.core :as aoc] [clojure.string :as str]
             [clojure.set :as set]))
 
+(defn new-recipes [score]
+  (->> score
+       (str)
+       (map int)
+       (map #(- % 48))))
+
+(defn play [max-r]
+  (loop [recipes [3 7] elf1 0 elf2 1]
+    ;; (println recipes elf1 elf2)
+    (if (>= (count recipes) (+ 10 max-r))
+      (apply str (take 10 (drop max-r recipes)))
+      (let [s1 (nth recipes elf1) s2 (nth recipes elf2)
+            sc (+ s1 s2)
+            nr (into recipes (new-recipes sc))
+            nn (count nr)
+            ]
+        (recur nr
+               (mod (+ 1 s1 elf1) nn)
+               (mod (+ 1 s2 elf2) nn))
+      )
+    )
+  ))
+
+;; part1: (play 556061) => "2107929416"
+
+(defn play2 [target]
+  (let [tseq (new-recipes target)]
+    (loop [recipes [3 7] elf1 0 elf2 1]
+      (if (>= (java.util.Collections/indexOfSubList recipes tseq) 0)
+        (java.util.Collections/indexOfSubList recipes tseq)
+        (let [s1 (nth recipes elf1) s2 (nth recipes elf2)
+              sc (+ s1 s2)
+              nr (into recipes (new-recipes sc))
+              nn (count nr)
+              ]
+          (recur nr
+                 (mod (+ 1 s1 elf1) nn)
+                 (mod (+ 1 s2 elf2) nn))
+      ))
+    )
+  ))
+
+;; part2: (play2 556061) =>
+
