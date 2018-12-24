aa8efa4918117d78981c6c300d78dd8b5adca2ab
Edd Wilder-James <edd@me.com>
2018-12-23 18:23:45 -0800

    maze parser

--- solutions/src/day15/core.clj
+++ solutions/src/day15/core.clj
@@ -2,3 +2,19 @@
   (:require [aoc.core :as aoc] [clojure.string :as str]
             [clojure.set :as set] [clojure.spec.alpha :as s]))
 
+
+(def inp (aoc/puzzle-lines "day15-ex1"))
+
+(defn parse-line [y l]
+  (loop [l l creatures [] r [] x 0]
+    (if (empty? l) [r creatures]
+        (let [c (first l)]
+          (if (#{\G \E} c) 
+            (recur (rest l) (conj creatures [[x y] c]) (conj r \.) (inc x))
+            (recur (rest l) creatures (conj r c) (inc x)))))))
+
+(defn parse-input [i]
+  (let [s (map-indexed parse-line i)
+        maze (map first s)
+        creatures (apply concat (map second s))]
+    [maze creatures]))
