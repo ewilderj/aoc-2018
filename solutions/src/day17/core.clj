b1741b35506bc0b765a95f31659eede0349d248a
Edd Wilder-James <edd@me.com>
2018-12-30 08:27:48 -0800

    maze parsing and debug functions

--- solutions/src/day17/core.clj
+++ solutions/src/day17/core.clj
@@ -22,8 +22,8 @@
                          (repeat c)))]
     (reduce (fn [r [p c]] (assoc r (vec p) c)) u cc)))
 
-(defn make-universe
-  "Turn a vector of input lines into the universe"
+(defn make-maze
+  "Turn a vector of input lines into the maze"
   [src]
 
   (loop [i src u (sorted-map)]
@@ -33,6 +33,35 @@
             (recur (rest i) (draw-vert u \# j a b))
             (recur (rest i) (draw-horiz u \# j a b)))))))
 
+(defn maze-cell
+  [maze point]
+  (get maze point \.))
+
 (defn traversable?
   [u p]
-  (contains? #{\. \|} (get u p \.)))
+  (contains? #{\. \|} (maze-cell u p)))
+
+(defn bounds
+  "Returns min-x, max-x and max-y of the maze. min-y is always 0"
+  [maze]
+  (vector (apply min (map ffirst maze))
+          (apply max (map ffirst maze))
+          (apply max (map (comp second first) maze))))
+
+(defn make-universe
+  [src]
+  (let [m (make-maze src)
+        [x1 x2 y] (bounds m)]
+    {:maze (assoc m [500 0] \+)
+     :min-x x1 :max-x x2 :max-y y}))
+
+(defn render-universe
+  [u]
+  (let [maze (u :maze)
+        [x1 x2 yy] (map u [:min-x :max-x :max-y])]
+    (str/join "\n"
+              (for [y (range 0 (inc yy))]
+                (apply str (for [x (range x1 (inc x2))]
+                             (maze-cell maze [x y])))))))
+
+(defn pud [u] (print (render-universe u)))
