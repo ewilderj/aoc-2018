8384ba2b9d69fa604b19a71991eb7c189a90ad71
Edd Wilder-James <edd@me.com>
2018-12-31 09:27:24 -0800

    Day 18 done

--- solutions/src/day18/core.clj
+++ solutions/src/day18/core.clj
@@ -1,7 +1,5 @@
 (ns day18.core
-  (:require [aoc.core :as aoc] [clojure.string :as str]
-            [clojure.set :as set] [clojure.spec.alpha :as s]))
-
+  (:require [aoc.core :as aoc] [clojure.string :as str]))
 
 (def adjacencies [[-1 -1] [0 -1] [1 -1] [-1 0] [1 0] [-1 1] [0 1] [1 1]])
 (defn v+ [a b] (mapv + a b))
@@ -24,18 +22,34 @@
       (and (= s \.) (>= (c \| 0) 3)) \|
       (and (= s \|) (>= (c \# 0) 3)) \#
       (= s \#) (if (and (> (c \# 0) 0) (> (c \| 0) 0)) \# \.)
-      :else s
-    )))
+      :else s)))
 
 (defn turn [m]
   (reduce (fn [r p] (assoc r p (decide m p))) {} (keys m)))
 
-(defn ms [m]
+(defn ms
+  "Debug function to turn map into string"
+  [m]
   (let [mx (apply max (map first (keys m)))
         my (apply max (map second (keys m)))]
     (str/join "\n" (for [y (range 0 (inc my))]
                      (apply str (for [x (range 0 (inc mx))] (m [x y])))))))
 
-(defn part1 [i]
-  (apply * (map (frequencies (vals (nth (iterate turn (make-maze i)) 10))) [\| \#])))
+(defn score [m]
+  (apply * (map (frequencies (vals m)) [\| \#])))
 
+(defn part1 [i]  ;; => 467819
+  (score (nth (iterate turn (make-maze i)) 10)))
+
+(defn find-cycle [results]
+  (loop [i 550 seen {}]      ;; doesn't cycle til at least 550, set to 0 for new data
+    (let [r (nth results i)]
+      (if (seen r)
+        [(seen r) i]
+        (recur (inc i) (assoc seen r i))))))
+
+(defn part2 [i]  ;; => 195305
+  (let [m (make-maze i)
+        results (iterate turn m)
+        [x y] (find-cycle results)]
+    (score (nth results (+ x (mod (- 1000000000 x) (- y x)))))))
